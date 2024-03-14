5d23f689fde25d07aba72c5143da782c251ac325
PavolMilcik <pavolmilcik@gmail.com>
2024-03-14 13:13:28 +0100

    Update upgraded Ice Cream java task.

--- src/ComplexBasicsOfJava/ComplexityInEasyLevelTasks/IceCream.java
+++ src/ComplexBasicsOfJava/ComplexityInEasyLevelTasks/IceCream.java
@@ -4,23 +4,24 @@ import java.util.Scanner;
 
 
 public class IceCream {
+
     public static int numberOfScoops() {
         Scanner myScanner = new Scanner(System.in);
         int userNumberOfScoops;
 
-        System.out.println("Hello.");
+        System.out.println("\nHello.");
         while (true) {
             System.out.println("Write the number of ice cream scoops:");
             try {
                 userNumberOfScoops = myScanner.nextInt();
-                if (userNumberOfScoops > 0 && userNumberOfScoops < 10) {
+                if (userNumberOfScoops > 0 && userNumberOfScoops <= 10) {
                     break;
                 } else {
                     System.out.println("You may write some scoops, but no more than 10.\n");
                     System.out.println("----- Try again.");
                 }
             } catch (Exception e) {
-                System.out.println("Write only number!\n");
+                System.out.println("Write only integer number!\n");
                 System.out.println("----- Try again.");
                 myScanner.nextLine();
             }
@@ -53,11 +54,11 @@ public class IceCream {
         String returnedString;
 
         if (scoops % 2 == 0) {
-            returnedString = "\n\nBecause you have an even number of scoops, the price for one scoop is "
+            returnedString = "\nBecause you have an even number of scoops, the price for one scoop is "
                     + evenPrice + " €.\n" + "You want " + scoops + " scoops of ice cream.\n" +
                     "--- So, the total price for your ice cream is " + price + " €.";
         } else {
-            returnedString = "\n\nBecause you have an odd number of scoops, the price for one scoop is "
+            returnedString = "\nBecause you have an odd number of scoops, the price for one scoop is "
                     + oddPrice + " €.\n" + "You want " + scoops + " scoops of ice cream.\n" +
                     "--- So, the total price for your ice cream is " + price + " €.";
         }
@@ -65,16 +66,12 @@ public class IceCream {
         return returnedString;
     }
 
-    public static void main(String[] args) {
-        System.out.println();
 
+    public static void main(String[] args) {
 
         int scoopNumbers = numberOfScoops();
         double iceCreamPrice = priceOfIceCream(scoopNumbers);
 
         System.out.println(returnIceCreamStory(scoopNumbers, iceCreamPrice));
-
-
-        System.out.println("\n");
     }
 }
