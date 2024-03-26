61a424ec701af345608ab410c205e6ff011f317c
PavolMilcik <pavolmilcik@gmail.com>
2024-03-26 15:59:28 +0100

    Add an enhanced version of the Are Anagrams And Sort Chars In Strings java task.

---
+++ src/BasicsOfJavaInDepth/EnhancedModerateLevelTasks/AreAnagramsAndSortChars.java
@@ -0,0 +1,117 @@
+package BasicsOfJavaInDepth.EnhancedModerateLevelTasks;
+
+
+import java.util.Arrays;
+
+
+public class AreAnagramsAndSortChars {
+
+    // ---------------------------------------------------------------------------------------
+    // ----------------------- ENHANCED VERSION of 'ARE ANAGRAMS?' task -----------------------
+    public static String[] sortCharsInStrings(String textX, String textY) {
+        // textX to char array:
+        char[] textXToArray = new char[textX.length()];
+        for (int i = 0; i < textX.length(); i++) {
+            textXToArray[i] = textX.charAt(i);
+        }
+
+        // textY to char array:
+        char[] textYToArray = new char[textY.length()];
+        for (int i = 0; i < textY.length(); i++) {
+            textYToArray[i] = textY.charAt(i);
+        }
+
+        // sort textX char array:
+        char currentIsBiggerCharX;
+        char nextCharX;
+        for (int i = 0; i < textXToArray.length; i++) {
+            for (int j = 0; j < textXToArray.length - 1; j++) {
+                currentIsBiggerCharX = textXToArray[j];
+                nextCharX = textXToArray[j + 1];
+                if (currentIsBiggerCharX > nextCharX) {
+                    textXToArray[j] = nextCharX;
+                    textXToArray[j + 1] = currentIsBiggerCharX;
+                }
+            }
+        }
+
+        // sort textY char array:
+        char currentIsBiggerCharY;
+        char nextCharY;
+        for (int i = 0; i < textYToArray.length; i++) {
+            for (int j = 0; j < textYToArray.length - 1; j++) {
+                currentIsBiggerCharY = textYToArray[j];
+                nextCharY = textYToArray[j + 1];
+                if (currentIsBiggerCharY > nextCharY) {
+                    textYToArray[j] = nextCharY;
+                    textYToArray[j + 1] = currentIsBiggerCharY;
+                }
+            }
+        }
+
+        // create string from char array:
+        StringBuilder s = new StringBuilder();
+        String sortedA;
+        for (char c : textXToArray) {
+            s.append(c);
+        }
+        sortedA = s.toString();
+        s = new StringBuilder();
+
+        // create string from char array:
+        String sortedB;
+        for (char c : textYToArray) {
+            s.append(c);
+        }
+        sortedB = s.toString();
+
+        return new String[]{sortedA, sortedB};
+    }
+
+    public static boolean checkIfAnagrams(String textX, String textY) {
+        String[] sortedTexts = sortCharsInStrings(textX, textY);
+        String sortedTextX = sortedTexts[0];
+        String sortedTextY = sortedTexts[1];
+
+        return sortedTextX.equals(sortedTextY);
+    }
+
+    // ---------------------------------------------------------------------------------------
+    // ----------------------- SHORTER VERSION of 'ARE ANAGRAMS?' task -----------------------
+    public static boolean areAnagrams(String textK, String textL) {
+        if (textK.length() != textL.length()) {
+            return false;
+        }
+
+        char[] charArrayTextK = textK.toCharArray();
+        char[] charArrayTextL = textL.toCharArray();
+
+        Arrays.sort(charArrayTextK);
+        Arrays.sort(charArrayTextL);
+
+        return Arrays.equals(charArrayTextK, charArrayTextL);
+    }
+
+
+    public static void main(String[] args) {
+        String stringA = "heart";
+        String stringB = "earth";
+        String stringC = "silent";
+        String stringD = "listen";
+        String stringE = "nose";
+        String stringF = "sore";
+
+        System.out.println("\n\nThis program checks if two strings are anagrams of each other " +
+                "\nand returns true or false as a result.\n");
+
+        System.out.println("--- Enhanced version of 'are anagrams' task:");
+        System.out.println(stringA + ", " + stringB + " - " + checkIfAnagrams(stringA, stringB));
+        System.out.println(stringC + ", " + stringD + " - " + checkIfAnagrams(stringC, stringD));
+        System.out.println(stringE + ", " + stringF + " - " + checkIfAnagrams(stringE, stringF));
+
+        System.out.println("\n--- Shorter version of 'are anagrams' task:");
+        System.out.println("cafe, face" + " - " + areAnagrams("cafe", "face"));
+        System.out.println("road, dore" + " - " + areAnagrams("road", "dore"));
+        System.out.println("angel, angle" + " - " + areAnagrams("angel", "angle"));
+    }
+}
