ec795b76fc0c17fd85ea085cc2c30aff0dbaea61
PavolMilcik <pavolmilcik@gmail.com>
2024-06-01 17:53:13 +0200

    Update of the Return Longest Palindrome Substring java task with another enhanced solution.

--- src/BasicsOfJavaInDepth/EnhancedModerateLevelTasks/LongestPalindromeSubstring.java
+++ src/BasicsOfJavaInDepth/EnhancedModerateLevelTasks/LongestPalindromeSubstring.java
@@ -30,14 +30,63 @@ public class LongestPalindromeSubstring {
     }
 
 
+    // ---------------------------------------------------------------------------------------
+    // -------- MY ANOTHER ENHANCED VERSION of the 'Longest Palindrome Substring' task --------
+    public static String longestPalindromeSubstring(String text) {
+        StringBuilder s = new StringBuilder();
+        String longestPalindrome = "";
+
+        for (int i = 0; i < text.length(); i++) {
+            for (int j = 0; j <= text.length() - i - 1; j++) {
+                if (text.charAt(i) == text.charAt(text.length() - 1 - j)) {
+                    int tempVar = i;
+                    for (int k = 0; k < text.length() - i - j; k++) {
+                        if (text.charAt(tempVar) == text.charAt(text.length() - 1 - k - j)) {
+                            s.append(text.charAt(tempVar));
+                            tempVar++;
+                        } else {
+                            s = new StringBuilder();
+                            break;
+                        }
+                    }
+                    if (longestPalindrome.isEmpty() || s.length() > longestPalindrome.length()) {
+                        longestPalindrome = s.toString();
+                    }
+                    s = new StringBuilder();
+                }
+                if (longestPalindrome.length() == text.length()) {
+                    break;
+                }
+            }
+        }
+
+        return longestPalindrome;
+    }
+
+
     public static void main(String[] args) {
 
-        System.out.println("\n\nThis function returns the longest palindromic substring in the given string.\n");
-
+        System.out.println("\n\n--- This function returns the longest palindromic substring in the given string ---");
         System.out.println("xzmadamfam - " + returnLongestPalindromeSubstring("xzmadamfam"));           // madam
         System.out.println("abba - " + returnLongestPalindromeSubstring("abba"));                       // abba
         System.out.println("abacada - " + returnLongestPalindromeSubstring("abacada"));                 // aba
         System.out.println("howexeyexyere - " + returnLongestPalindromeSubstring("howexeyexyere"));     // xeyex
         System.out.println("kisdeyezxcxc - " + returnLongestPalindromeSubstring("kisdeyezxcxc"));       // eye
+
+
+        System.out.println("\n\n--- MY ANOTHER ENHANCED VERSION of the 'Longest Palindrome Substring' task ---");
+        System.out.println("eye - " + longestPalindromeSubstring("eye"));                       // eye
+        System.out.println("madam - " + longestPalindromeSubstring("madam"));                   // madam
+        System.out.println("abba - " + longestPalindromeSubstring("abba"));                     // abba
+        System.out.println("abacada - " + longestPalindromeSubstring("abacada"));               // aba
+        System.out.println("zbacaba - " + longestPalindromeSubstring("zbacaba"));               // bacab
+        System.out.println("xzmadamfam - " + longestPalindromeSubstring("xzmadamfam"));         // madam
+        System.out.println("howexeyexyerey - " + longestPalindromeSubstring("howexeyexyerey")); // xeyex
+        System.out.println("deyecxc - " + longestPalindromeSubstring("deyecxc"));               // eye
+        System.out.println("kisdeyezxcxc - " + longestPalindromeSubstring("kisdeyezxcxc"));     // eye
+        System.out.println("eyezxcxc - " + longestPalindromeSubstring("eyezxcxc"));             // eye
+        System.out.println("a - " + longestPalindromeSubstring("a"));                           // a
+        System.out.println("aa - " + longestPalindromeSubstring("aa"));                         // aa
+        System.out.println("aba - " + longestPalindromeSubstring("aba"));                       // aba
     }
 }
