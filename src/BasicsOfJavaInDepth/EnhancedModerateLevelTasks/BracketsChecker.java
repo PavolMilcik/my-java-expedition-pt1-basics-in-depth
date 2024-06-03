b39e3e3230c180f4f8dba9baa347ac24e7dbf212
PavolMilcik <pavolmilcik@gmail.com>
2024-06-03 17:28:06 +0200

    Update of the Brackets Checker java task with another solution.

--- src/BasicsOfJavaInDepth/EnhancedModerateLevelTasks/BracketsChecker.java
+++ src/BasicsOfJavaInDepth/EnhancedModerateLevelTasks/BracketsChecker.java
@@ -51,6 +51,54 @@ public class BracketsChecker {
     }
 
 
+    // ---------------------------------------------------------------------------------------
+    // ------------ MY ANOTHER VERSION of the 'Brackets Checker' java task -------------------
+    public static boolean validBracketsXYZ(String s) {
+        char[] brackets = s.toCharArray();
+        // x = ()
+        int x = 0;
+        // y = []
+        int y = 0;
+        // z = {}
+        int z = 0;
+
+        for (char bracket : brackets) {
+            if (bracket == '(' || bracket == ')') {
+                if (bracket == '(') {
+                    x++;
+                } else {
+                    x--;
+                }
+                if (x < 0) {
+                    return false;
+                }
+            }
+            if (bracket == '[' || bracket == ']') {
+                if (bracket == '[') {
+                    y++;
+                } else {
+                    y--;
+                }
+                if (y < 0) {
+                    return false;
+                }
+            }
+            if (bracket == '{' || bracket == '}') {
+                if (bracket == '{') {
+                    z++;
+                } else {
+                    z--;
+                }
+                if (z < 0) {
+                    return false;
+                }
+            }
+        }
+
+        return x == 0 && y == 0 && z == 0;
+    }
+
+
     public static void main(String[] args) {
         System.out.println("\n1. ------------- Check that the brackets are correctly closed. ---------");
 
@@ -80,5 +128,33 @@ public class BracketsChecker {
         System.out.println("[ - " + validBrackets("["));                             // false
         System.out.println("[) - " + validBrackets("[)"));                           // false
         System.out.println("({([()()])})) - " + validBrackets("({([()()])}))"));     // false
+
+
+        System.out.println("\n\n------- MY ANOTHER VERSION of the 'Brackets Checker' java task -------");
+        System.out.println("3. ------------- Check that the brackets are correctly closed. ---------");
+        System.out.println("() - " + validBracketsXYZ("()"));              // true
+        System.out.println("()() - " + validBracketsXYZ("()()"));          // true
+        System.out.println("(()) - " + validBracketsXYZ("(())"));          // true
+        System.out.println("(()()) - " + validBracketsXYZ("(()())"));      // true
+
+        System.out.println("\n)( - " + validBracketsXYZ(")("));            // false
+        System.out.println("()) - " + validBracketsXYZ("())"));            // false
+        System.out.println("((() - " + validBracketsXYZ("((()"));          // false
+        System.out.println("((() - " + validBracketsXYZ("())(()"));        // false
+        System.out.println("(()))) - " + validBracketsXYZ("(())))"));      // false
+
+        System.out.println("\n[{]} - " + validBracketsXYZ("[{]}"));                                 // true
+        System.out.println("'' - " + validBracketsXYZ(""));                                       // true
+        System.out.println("([][]{()}) - " + validBracketsXYZ("([][]{()})"));                     // true
+        System.out.println("({([()()])}) - " + validBracketsXYZ("({([()()])})"));                 // true
+        System.out.println("({([()()])})()[][{]} - " + validBracketsXYZ("({([()()])})()[][{]}")); // true
+
+        System.out.println("\n}{ - " + validBracketsXYZ("}{"));                         // false
+        System.out.println("{) - " + validBracketsXYZ("{)"));                           // false
+        System.out.println("[}]{ - " + validBracketsXYZ("[}]{"));                       // false
+        System.out.println("[{]{ - " + validBracketsXYZ("[{]{"));                       // false
+        System.out.println("[ - " + validBracketsXYZ("["));                             // false
+        System.out.println("[) - " + validBracketsXYZ("[)"));                           // false
+        System.out.println("({([()()])})) - " + validBracketsXYZ("({([()()])}))"));     // false
     }
 }
